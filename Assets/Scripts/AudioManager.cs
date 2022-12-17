45a247c672219bafd77bd467a9ac028bec2deb10
Chen <xj19971@gmail.com>
2022-12-18 00:50:39 +0800

    提交一下音效

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class AudioManager : MonoSingleton<AudioManager>
+{
+    public AudioSource bgMusic;
+
+    public void ChangeBgMusic(string music)
+    {
+        var clip = Resources.Load<AudioClip>($"Sound/{music}");
+        bgMusic.clip = clip;
+        bgMusic.Play();
+    }
+    
+    public void PlaySound(string sound)
+    {
+        var prefab = Utility.GetPrefab("SoundSource");
+        var player = Instantiate(prefab).GetComponent<SoundPlayer>();
+        player.PlaySound(sound);
+    }
+}
