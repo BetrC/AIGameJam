e64da129857cb093f974af169ad73cddf628b108
Zeyong Shan <danaini13@gmail.com>
2022-12-18 01:16:57 +0800

    随机加权重

--- Server/PokemonManager.py
+++ Server/PokemonManager.py
@@ -1,8 +1,11 @@
+import math
 import random
 
 class PokemonManager():
     def LoadData(self):
         self.pokemon_list = []
+        self.weight_list = []
+        self.total_weight = 0
         with open('PokemonData/pokemon_data.txt', 'r') as config_file:
             count = int(config_file.readline())
             for index in range(0, count):
@@ -20,18 +23,32 @@ class PokemonManager():
                     key = parts[0]
                     value = parts[1]
                     pokemonInfo[key] = value[:-1].strip()
+                    if key == 'Attack':
+                        weight = math.ceil((1.0/math.pow(int(value), 1.5))*1000000)
+                        self.weight_list.append(weight)
                 self.pokemon_list.append(pokemonInfo)
+        for weight in self.weight_list:
+            self.total_weight += weight
         print(self.pokemon_list)
-        # for x in range(1, count):
-        #     for y in range(1, count):
-        #         self.simulate_battle_steps(x, y)
+        print(self.weight_list)
+        print(self.total_weight)
         print("loadDataFinished!")
 
     def random_pokemon(self):
-        return self.pokemon_list[random.randint(0, len(self.pokemon_list) - 1)]
+        return self.get_pokemon_info_by_id(self.random_pokemon_id())
 
     def random_pokemon_id(self):
-        return random.randint(0, len(self.pokemon_list) - 1)
+        randomWeight = random.randint(0, self.total_weight)
+        currentWeight = 0
+        chooseIndex = 0
+        for weight in self.weight_list:
+            currentWeight += weight
+            if randomWeight <= currentWeight:
+                break
+            chooseIndex += 1
+        if chooseIndex >= 20:
+            chooseIndex = 19
+        return chooseIndex + 1
 
     def get_pokemon_info_by_id(self, pokemonId):
         pokemonId -= 1
