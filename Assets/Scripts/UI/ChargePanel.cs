1f3f82b403521c9ef479c03fba77fba708e0cd8c
Chen <xj19971@gmail.com>
2022-12-18 15:33:37 +0800

    提交一下

--- Assets/Scripts/UI/ChargePanel.cs
+++ Assets/Scripts/UI/ChargePanel.cs
@@ -1,7 +1,24 @@
+using DG.Tweening;
+using UnityEngine.UI;
+
 public class ChargePanel : UIPanelBase
 {
+    public Image faceImg;
+
     public override void OnShow()
     {
+        var seq = DOTween.Sequence();
+        seq.AppendInterval(1f).AppendCallback(() =>
+            {
+                faceImg.gameObject.SetActive(true);
+            }).AppendInterval(2f)
+            .AppendCallback((() =>
+            {
+                GameManager.Instance.Coin += 648;
+                Utility.ShowHint("获得648游戏币");
+                faceImg.gameObject.SetActive(false);
+                Close();
+            }));
     }
 
     public override void OnHide()
